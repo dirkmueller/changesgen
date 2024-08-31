9d94ec5a8f6f01413ba539d398ffc8b74e29e8c5
Dirk Müller <dirk@dmllr.de>
2024-08-31 16:47:03 +0200

    Slightly nicer formatting, add linting

--- changesgen.py
+++ changesgen.py
@@ -23,18 +23,18 @@ import argparse
 import configparser
 import glob
 import logging as LOG
-import time
 import os
 import re
-from subprocess import Popen, PIPE, STDOUT
 import tarfile
 import textwrap
+import time
 import urllib.parse
+from subprocess import PIPE, STDOUT, Popen
 
-from docutils.core import publish_parts
-from bs4 import BeautifulSoup
-import requests
 import packaging.version as pv
+import requests
+from bs4 import BeautifulSoup
+from docutils.core import publish_parts
 
 NEWRELEASES_API_KEY = None
 
@@ -50,10 +50,7 @@ def parse_from_spec_file(path):
         return pkg_info
 
     parsed_spec = Popen(
-        ('rpmspec', '-P', primary_spec[0]),
-        stdout=PIPE,
-        stderr=STDOUT,
-        text=True,
+        ('rpmspec', '-P', primary_spec[0]), stdout=PIPE, stderr=STDOUT, text=True
     )
     specfile = parsed_spec.communicate()[0].split('\n')
 
@@ -88,9 +85,7 @@ def parse_from_spec_file(path):
                 )
 
             if 'github.com' == gh_url.netloc:
-                pkg_info['github_project'] = '/'.join(
-                    gh_url.path.split('/')[1:3]
-                )
+                pkg_info['github_project'] = '/'.join(gh_url.path.split('/')[1:3])
 
     return pkg_info
 
@@ -101,15 +96,10 @@ def req_addnewrelease(provider, path):
     resp = requests.post(
         'https://api.newreleases.io/v1/projects',
         headers={'X-Key': NEWRELEASES_API_KEY},
-        json={
-            'provider': provider,
-            'name': path,
-            'email_notification': 'instant',
-        },
+        json={'provider': provider, 'name': path, 'email_notification': 'instant'},
     )
     LOG.info(
-        'adding new release for provider '
-        f'{provider}/{path}: {resp.status_code}'
+        'adding new release for provider ' f'{provider}/{path}: {resp.status_code}'
     )
     return resp.status_code, resp.json()
 
@@ -119,8 +109,7 @@ def req_newreleases(path):
 
     LOG.debug(f'requesting newreleases: {path}')
     resp = requests.get(
-        f'https://api.newreleases.io/v1/{path}',
-        headers={'X-Key': NEWRELEASES_API_KEY},
+        f'https://api.newreleases.io/v1/{path}', headers={'X-Key': NEWRELEASES_API_KEY}
     )
     resp.raise_for_status()
     return resp.status_code, resp.json()
@@ -161,9 +150,7 @@ def changes_to_text(changes):
             r = m.group(1)
 
     r = '\n'.join(
-        textwrap.wrap(
-            r, width=65, initial_indent='  * ', subsequent_indent='    '
-        )
+        textwrap.wrap(r, width=65, initial_indent='  * ', subsequent_indent='    ')
     )
 
     LOG.debug(f'changes_to_text: converted {changes} to {r}')
@@ -252,22 +239,13 @@ def extract_changes_from_github_release(github_path, oldv, newv):
         try:
             relver = pv.parse(release_version)
             if relver > start_relversion:
-                LOG.debug(
-                    f'skipping over {release_version} > {start_relversion}'
-                )
+                LOG.debug(f'skipping over {release_version} > {start_relversion}')
                 continue
             if relver.major < stop_relversion.major:
-                LOG.debug(
-                    f'skipping over {release_version} < {stop_relversion}'
-                )
+                LOG.debug(f'skipping over {release_version} < {stop_relversion}')
                 continue
-            if (
-                relver.major == stop_relversion.major
-                and relver <= stop_relversion
-            ):
-                LOG.debug(
-                    f'stopping at {release_version} <= {stop_relversion}'
-                )
+            if relver.major == stop_relversion.major and relver <= stop_relversion:
+                LOG.debug(f'stopping at {release_version} <= {stop_relversion}')
                 break
         except pv.InvalidVersion:
             if release_version in (oldv, f'v{oldv}'):
@@ -282,9 +260,7 @@ def extract_changes_from_github_release(github_path, oldv, newv):
                 summary += '- '
             summary += f'update to {release_version}:\n'
             for line in (
-                BeautifulSoup(versionnote, features='lxml')
-                .get_text()
-                .split('\n')
+                BeautifulSoup(versionnote, features='lxml').get_text().split('\n')
             ):
                 summary += md_to_text(line) + '\n'
     return summary
@@ -295,9 +271,7 @@ def extract_changes_from_newreleases(github_path, oldv, newv):
     summary = ''
 
     while True:
-        status, resp = req_newreleases(
-            f'projects/github/{github_path}/releases'
-        )
+        status, resp = req_newreleases(f'projects/github/{github_path}/releases')
         if status == 404:
             status, _ = req_addnewrelease('github', github_path)
             time.sleep(1)
@@ -326,6 +300,39 @@ def extract_changes_from_newreleases(github_path, oldv, newv):
                 ):
                     summary += changes_to_text(line) + '\n'
     return summary
+
+
+def extract_update_section(oldv, newv, package_name, infile):
+    """Extract from the given text the update section.
+
+    We assume that the relevant part starts with {newv} somewhere isolated and ends
+    with {oldv}. e.g. with other words we assume reverse chronological ordering."""
+    inupdatesection = False
+    update_section: str = ''
+    for line in infile:
+        line: str = line.decode(encoding='utf-8', errors='ignore')
+        if inupdatesection:
+            stripped_line: str = line.strip(' \r\n()[]t*#-=:/`')
+            if stripped_line:
+                # packagename oldversion (releasedate)
+                if stripped_line.lower().startswith(package_name.lower()):
+                    stripped_line = stripped_line.partition(' ')[2].strip()
+                if (
+                    stripped_line.startswith(oldv)
+                    or stripped_line.startswith(f'({oldv})')
+                    or stripped_line.lower().startswith(f'version {oldv}')
+                    or stripped_line.endswith(oldv)
+                    or stripped_line.endswith(f'{oldv}.0')
+                    or ('release' in stripped_line.lower() and oldv in stripped_line)
+                ):
+                    break
+            update_section += line
+            continue
+
+        if not inupdatesection and newv in line:
+            update_section += line
+            inupdatesection = True
+    return update_section
 
 
 def extract_changes_from_tarball(package_information, oldv, newv):
@@ -363,83 +370,31 @@ def extract_changes_from_tarball(package_information, oldv, newv):
                     if not finfo.isfile():
                         continue
                     name = finfo.name
-                    if (
-                        name.rpartition('/')[2].casefold()
-                        == candidate.casefold()
-                    ):
+                    if name.rpartition('/')[2].casefold() == candidate.casefold():
                         LOG.debug(f'found changes file: {candidate}')
-                        inupdatesection = False
-                        update_section: str = ''
-                        for line in source.extractfile(name):
-                            line = line.decode(
-                                encoding='utf-8', errors='ignore'
-                            )
-                            if inupdatesection:
-                                stripped_line: str = line.strip(
-                                    ' \r\n()[]t*#-=:/`'
-                                )
-                                if stripped_line:
-                                    # packagename oldversion (releasedate)
-                                    if stripped_line.lower().startswith(
-                                        package_name.lower()
-                                    ):
-                                        stripped_line = (
-                                            stripped_line.partition(' ')[
-                                                2
-                                            ].strip()
-                                        )
-                                    if (
-                                        stripped_line.startswith(oldv)
-                                        or stripped_line.startswith(
-                                            f'({oldv})'
-                                        )
-                                        or stripped_line.lower().startswith(
-                                            f'version {oldv}'
-                                        )
-                                        or stripped_line.endswith(oldv)
-                                        or stripped_line.endswith(f'{oldv}.0')
-                                        or (
-                                            'release' in stripped_line.lower()
-                                            and oldv in stripped_line
-                                        )
-                                    ):
-                                        break
-                                update_section += line
-                                continue
-
-                            if not inupdatesection and newv in line:
-                                update_section += line
-                                inupdatesection = True
-
+                        update_section: str = extract_update_section(
+                            oldv, newv, package_name, source.extractfile(name)
+                        )
                         changes: str = ''
                         if name.rpartition('.')[2].lower() in ('rst',):
                             changes = rst_to_text(rst=update_section)
-                        elif name.rpartition('.')[2].lower() in (
-                            'md',
-                            'adoc',
-                        ):
+                        elif name.rpartition('.')[2].lower() in ('md', 'adoc'):
                             changes = md_to_text(update_section)
                         else:
                             for line in update_section.split('\n'):
-                                changes += (
-                                    changes_to_text(line).rstrip() + '\n'
-                                )
+                                changes += changes_to_text(line).rstrip() + '\n'
 
                         changes = changes.rstrip() + '\n'
                         if len(changes) > 4:
                             print(f"update to {newv}:\n{''.join(changes)}")
                             return True
-                        pass
-                pass
     return False
 
 
 def main():
     """Main function"""
 
-    with open(
-        os.path.expanduser('~/.config/changesgenrc'), encoding='utf8'
-    ) as f:
+    with open(os.path.expanduser('~/.config/changesgenrc'), encoding='utf8') as f:
         global NEWRELEASES_API_KEY
         c = configparser.ConfigParser(strict=False)
         c.read_file(f)
@@ -449,12 +404,8 @@ def main():
         description='Generate OSC vc changes', exit_on_error=False
     )
     parse.add_argument('-d', '--debug', action='store_true')
-    parse.add_argument(
-        'old', metavar='oldv', type=str, help='Old version', nargs='?'
-    )
-    parse.add_argument(
-        'new', metavar='newv', type=str, help='New version', nargs='?'
-    )
+    parse.add_argument('old', metavar='oldv', type=str, help='Old version', nargs='?')
+    parse.add_argument('new', metavar='newv', type=str, help='New version', nargs='?')
 
     args = parse.parse_args()
 
@@ -464,9 +415,7 @@ def main():
     package_information = parse_from_spec_file(os.getcwd())
 
     if 'version' not in package_information:
-        LOG.fatal(
-            'Cannot determine starting version (not run in osc checkout?)'
-        )
+        LOG.fatal('Cannot determine starting version (not run in osc checkout?)')
         return
 
     oldv = newv = package_information['version']
